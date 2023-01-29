8109c82dca6a2ca321fb9cc5a9095ae58edff9c1
pickyko <68360714+pickyko@users.noreply.github.com>
2023-01-29 17:17:03 -0600

    Add files via upload

---
+++ config.py
@@ -0,0 +1,5 @@
+profile_url = ""
+default_wait = 180
+LARGE_TXT = "Tracking music with last.fm"
+
+client_id = "1069384543481647174"
---
+++ main.py
@@ -0,0 +1,73 @@
+from pypresence import Presence
+import time
+import requests
+from bs4 import BeautifulSoup
+
+from config import *
+
+class LastPresence:
+    def __init__(self):
+        self.client = Presence(client_id)
+
+    def start(self):
+        self.client.connect()
+        self.loop()
+
+    def scrape(self):
+        """Fetches the song data from last.fm"""
+
+        # fetch profile page
+        print('fetching song information')
+        self.r = requests.get(profile_url)
+        self.soup = BeautifulSoup(self.r.text, 'lxml')
+
+        # find song information
+        self.current = self.soup.find_all("tr")[1]
+        self.name = self.current.find('td', class_='chartlist-name').find('a')
+        self.title = self.name.attrs['title']
+        self.href = "https://www.last.fm" + self.name.attrs['href']
+        self.artist = self.current.find('td', class_='chartlist-artist').find('a').attrs['title']
+        if "Scrobbling now" in self.current.find('span', class_= "chartlist-now-scrobbling").find('a').text:
+            self.srobbling = True
+        else:
+            self.scrobbling = False
+        #print('title is ' + str(title))
+        #print('artist is ' + str(artist))
+        time.sleep(5)
+
+        # now try to find the song length
+        self.r = requests.get(self.href)
+        self.soup = BeautifulSoup(self.r.text, 'lxml')
+        try:
+            self.soup.find('dd', class_='catalogue-metadata-description').string.strip()
+        except AttributeError:
+            self.length = None
+        else:
+            self.length = self.soup.find('dd', class_='catalogue-metadata-description').string.strip()
+            self.length = self.length.split(":")
+            self.length = int(self.length[0]) * 60 + int(self.length[1])
+        print('Current artist: ' + str(self.artist))
+        print('Current title: ' + str(self.title))
+
+    def cooldown(self):
+        # wait before fetching last.fm again
+        if self.length:
+            print('waiting ' + str(self.length + 5))
+            time.sleep(self.length + 5)
+        else:
+            print('waiting ' + str(default_wait))
+            time.sleep(default_wait)
+
+    def loop(self):
+        self.scrape()
+        self.client.update(large_image="large_img",
+            large_text=LARGE_TXT,
+            details="Playing a song",
+            state=f'{self.artist}: {self.title}')
+        self.cooldown()
+        self.loop()
+
+if __name__ == "__main__":
+    updater = LastPresence()
+    updater.start()
+
