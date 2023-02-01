0c3f764b35e3154963f8f44ea9db4aa75cc28256
pickyko <68360714+pickyko@users.noreply.github.com>
2023-02-01 12:11:43 -0600

    Add files via upload
    added saving of song lengths so don't have to make a request if don't need to

--- main.py
+++ main.py
@@ -10,14 +10,25 @@ class LastPresence:
         self.client = Presence(client_id)
 
     def start(self):
+        # get song duration information
+        with open("lengths.txt","r") as f:
+            self.original = f.read()
+            self.lines = [y for y in [x.strip() for x in self.original.split("\n\n")] if y != '']
+        self.lengths = {}
+        for x in self.lines:
+            self.lengths[x.split("\n")[0]] = x.split("\n")[1]
+        # make a backup
+        self.write_duration("lengths.txt~")
+        print("Fetched old song duration information")
         self.client.connect()
+        print("Connected to discord\n")
         self.loop()
 
     def scrape(self):
         """Fetches the song data from last.fm"""
 
         # fetch profile page
-        print('fetching song information')
+        print('Fetching song information')
         self.r = requests.get(profile_url)
         self.soup = BeautifulSoup(self.r.text, 'lxml')
 
@@ -36,31 +47,59 @@ class LastPresence:
         """
         time.sleep(1)
 
-        # now try to find the song length
-        self.r = requests.get(self.href)
-        self.soup = BeautifulSoup(self.r.text, 'lxml')
-        try:
-            self.soup.find('dd', class_='catalogue-metadata-description').string.strip()
-        except AttributeError:
-            self.length = None
-        else:
-            self.length = self.soup.find('dd', class_='catalogue-metadata-description').string.strip()
-            self.length = self.length.split(":")
-            self.length = int(self.length[0]) * 60 + int(self.length[1])
         print('Current artist: ' + str(self.artist))
         print('Current title: ' + str(self.title))
+
+    def find_duration(self):
+        """Finds the song duration either from the file or last.fm"""
+        try:
+            # check if length is stored in file
+            self.lengths[self.artist + self.title]
+        except:
+            # scrape song length
+            print("No old song duration found, fetching from last.fm")
+            self.r = requests.get(self.href)
+            self.soup = BeautifulSoup(self.r.text, 'lxml')
+            try:
+                self.soup.find('dd', class_='catalogue-metadata-description').string.strip()
+            except AttributeError:
+                self.length = None
+                self.lengths[self.artist + self.title] = "None"
+            else:
+                # find length
+                self.length = self.soup.find('dd', class_='catalogue-metadata-description').string.strip()
+                self.length = self.length.split(":")
+                self.length = int(self.length[0]) * 60 + int(self.length[1])
+                # store length
+                self.lengths[self.artist + self.title] = self.length
+            finally:
+                self.write_duration("lengths.txt")
+                print("Wrote new song duration")
+        else:
+            print("Found old song duration")
+            if self.lengths[self.artist + self.title] == "None":
+                self.length = None
+            else:
+                self.length = int(self.lengths[self.artist + self.title])
+
+    def write_duration(self, name):
+        """Writes durations to file"""
+        with open(name,"w") as f:
+            for x in self.lengths:
+                f.write(x + "\n" + str(self.lengths[x]) + "\n\n")
 
     def cooldown(self):
         # wait before fetching last.fm again
         if self.length:
-            print('waiting ' + str(self.length))
+            print('waiting ' + str(self.length) + '\n')
             time.sleep(self.length)
         else:
-            print('waiting ' + str(default_wait))
+            print('waiting ' + str(default_wait) + '\n')
             time.sleep(default_wait)
 
     def loop(self):
         self.scrape()
+        self.find_duration()
         self.client.update(large_image="large_img",
                         large_text=LARGE_TXT,
                         details="Playing a song",
