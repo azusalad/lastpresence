71d984d9f1912d331a5d3fed9c993ef059df7a49
pickyko <68360714+pickyko@users.noreply.github.com>
2023-02-06 17:52:29 -0600

    Update config.py

--- config.py
+++ config.py
@@ -1,5 +1,5 @@
 # Your last.fm profile url.  https://www.last.fm/user/yourusername
-profile_url = ""
+profile_url = "https://www.last.fm/user/"
 # If a song does not have a duration on last.fm, wait this many seconds before fetching again
 default_wait = 60
 # Large text
